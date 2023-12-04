89c03c69c1783e5534463cf0d91d69ce4de26ef8
unknown <putrigsi30a@gmail.com>
2023-12-04 13:47:13 +0700

    perbaikaan

--- resources/views/pengunjung/index.blade.php
+++ resources/views/pengunjung/index.blade.php
@@ -7,8 +7,8 @@
         class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
         <main>
             <div class="container selamat">
-                <h1>SELAMAT DATANG !!!</h1>
-                <h5>Sistem Informasi Perpustakaan Pengadilan Militer I-04 Palembang</h5>
+                <h1>SELAMAT DATANG</h1>
+                <h5>Layanan Informasi Perpustakaan Pengadilan Militer I-04 Palembang</h5>
             </div>
         </main>
     </section>
