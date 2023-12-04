e2b5e296727d0e525c56b4fa9c1420e68f9d2489
unknown <putrigsi30a@gmail.com>
2023-12-04 09:39:27 +0700

    perbaikaan

--- resources/views/pengunjung/katalogbuku.blade.php
+++ resources/views/pengunjung/katalogbuku.blade.php
@@ -35,7 +35,8 @@
                                             <td>{{ $buku->jumlah }} Buku</td>
                                             <td>Rak {{ $buku->raks['rak'] }}</td>
                                             <td>
-                                                <a href="/detailpengunjung/{{ $buku->id }}" class="btn bg-btn">Detail</a>
+                                                <a href="/detailpengunjung/{{ $buku->id }}"
+                                                    class="btn bg-btn">Detail</a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -51,34 +52,6 @@
                     <p>Akses Katalog Publik Daring - Gunakan fasilitas pencarian untuk mempercepat penemuan data katalog</p>
                 </div>
             </div>
-            {{-- <div class="katalogbuku">
-                <h2>Katalog Buku</h2>
-                <hr>
-            </div>
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="row">
-                        @foreach ($bukus as $buku)
-                            <div class="col-md-4 mb-4">
-                                <div class="card katalog">
-                                    <img src="{{ asset('storage/' . $buku->gambar) }}" alt="" width="100%"
-                                        height="370px">
-                                    <div class="card-body">
-                                        <a href="/detailpengunjung/{{ $buku->id }}" class="btn btn-primary">Detail
-                                            Buku</a>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-
-                </div>
-                <div class="col-3 info">
-                    <h4 class="card-header">INFORMASI</h4>
-                    <hr>
-                    <p>Akses Katalog Publik Daring - Gunakan fasilitas pencarian untuk mempercepat penemuan data katalog</p>
-                </div>
-            </div> --}}
         </div>
 
     </section>
